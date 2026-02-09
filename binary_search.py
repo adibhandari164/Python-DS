c8164ac782d10fbf7b3d1fe111a40d0c2246674e
adigunner47 <adityabhandari164@gmail.com>
2026-02-09 14:40:59 -0500

    changes

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,30 @@
-from unittest import result
+#Iterative solution: Solution implemented using loop structure of some kind
+#Space complexity is constant time. Complexity i.e big O notation talks about
+#the complexity of algorithm as function of input size 'n'. Hence in this case, memory allocation
+#doesn't change as function of n.
+def binary_search(list, target):
 
+    #The first and last is inclusive here
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        mid = (first + last)//2
+
+        if list[mid] ==  target:
+            return mid
+        elif target < list[mid]:
+            last = mid - 1
+        else:
+            first = mid + 1
+    
+    return None
 
 
 #Recursive function always need a stopping condition for it to end. 
-#Make sure all starting points coverge to that stopping conditions. 
+#Make sure all starting points coverge to that stopping conditions.
+# Space complexity: Here as n increases, the recursive call increases
+# and new variables keeps initializing for every new recursive function call.
 def recursive_binary_search(list, target):
     list_length = len(list)
     mid_element = list[list_length//2]
@@ -24,24 +45,6 @@ def recursive_binary_search(list, target):
         else:
             return val + list_length//2 +1
 
-
-def binary_search(list, target):
-
-    #The first and last is inclusive here
-    first = 0
-    last = len(list) - 1
-
-    while first <= last:
-        mid = (first + last)//2
-
-        if list[mid] ==  target:
-            return mid
-        elif target < list[mid]:
-            last = mid - 1
-        else:
-            first = mid + 1
-    
-    return None
 
 
 result = binary_search([1, 2, 3, 4, 5, 6, 7, 67, 100], 2)
